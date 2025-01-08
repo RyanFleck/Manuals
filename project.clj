846ff444a9a0c6bbe782400e9939aaa0b3ac22e8
Ryan Fleck <Ryan.Fleck@protonmail.com>
2025-01-08 08:05:21 -0700

    Add some Clojure stuff.

--- project.clj
+++ project.clj
@@ -3,5 +3,19 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.cli "1.0.214"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [mount "0.1.16"]
+                 [cprop "0.1.19"]
+                 ;; [bnbeckwith.com/orgmode "0.7.5"]
+                 [clojurewerkz/quartzite "2.2.0"]
+                 [org.immutant/immutant "2.1.10"]
+                 [overtone/at-at "1.3.58"]
+                 [clojure.java-time "1.4.2"]
+                 [environ "1.2.0"]
+                 [clj-http "3.13.0"]
+                 [org.clojure/data.json "2.5.0"]
+                 [nrepl "1.3.0"]]
+  :plugins [[dev.weavejester/lein-cljfmt "0.12.0"]]
   :repl-options {:init-ns org.core})
---
+++ src/org/core.clj
@@ -0,0 +1,3 @@
+(ns org.core)
+
+(defn helper [] (println "Hello!"))
