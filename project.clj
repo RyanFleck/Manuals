1ba61d8e9afa676ee4c642981e66f570f108f9cd
Ryan Fleck <Ryan.Fleck@protonmail.com>
2025-01-08 08:21:27 -0700

    Ensure REPL loads org.core ns

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.cli "1.0.214"]
                  [org.clojure/tools.logging "1.2.4"]
@@ -18,4 +19,5 @@
                  [org.clojure/data.json "2.5.0"]
                  [nrepl "1.3.0"]]
   :plugins [[dev.weavejester/lein-cljfmt "0.12.0"]]
+  :profiles {:dev {:resource-paths ["content"]}}
   :repl-options {:init-ns org.core})
