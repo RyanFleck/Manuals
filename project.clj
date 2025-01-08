59bec44d2f548579bad5ee76597d4b73dfe61606
Ryan Fleck <Ryan.Fleck@protonmail.com>
2025-01-08 09:55:01 -0700

    Delete child project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject org "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://manuals.ryanfleck.ca/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :source-paths ["src" "dev" "content"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/tools.cli "1.0.214"]
                  [org.clojure/tools.logging "1.2.4"]
                  [mount "0.1.16"]
@@ -17,7 +17,10 @@
                  [environ "1.2.0"]
                  [clj-http "3.13.0"]
                  [org.clojure/data.json "2.5.0"]
-                 [nrepl "1.3.0"]]
-  :plugins [[dev.weavejester/lein-cljfmt "0.12.0"]]
-  :profiles {:dev {:resource-paths ["content"]}}
-  :repl-options {:init-ns org.core})
+                 [cider/cider-nrepl "0.51.1"]
+                 [nrepl "1.3.1"]]
+  :plugins [[dev.weavejester/lein-cljfmt "0.12.0"]
+            [nrepl/lein-nrepl "0.3.2"]]
+  :repl-options {:greeting "Ready to hack in clj.org!"
+                 :init (do (require 'org.core) (org.core/helper))
+                 :init-ns org.core})
--- src/org/core.clj
+++ src/org/core.clj
@@ -1,3 +1,8 @@
-(ns org.core)
+(ns org.core
+  (:require [clj-http.client :as client]
+            [clojure.string :as s])
+  (:gen-class))
 
 (defn helper [] (println "Hello!"))
+
+(defn get [link] (client/get link))
